c3fdc5f8b4d10a8f32b2fd7589e08743ab90a40c
Tommy Nguyen <lqkiwinstechnicianchair@gmail.com>
2019-12-22 21:17:36 -0800

    alumni-page

--- contact-form.php
+++ contact-form.php
@@ -1,17 +1,15 @@
 <?php
 
-
 if (isset($_POST['submit'])) {
   $name = $_POST['name'];
-  $mailFrom = $_POST['email'];
   $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
   $message = $_POST['message'];
 
-  $mailTo = "lqkiwinstechnicianchair@gmail.com";
+  $mailTo = "lolitstommy@yahoo.com";
   $headers = "From: ".$mailFrom;
   $txt = "You have received an e-mail from ".$name.".\n\n".$message;
 
   mail($mailTo, $subject, $txt, $headers);
   header("Location: contact.html?mailsend");
 }
-?>
